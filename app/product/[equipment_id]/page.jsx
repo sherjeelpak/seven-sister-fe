9290bddd95718c24b3308958c46705d5ad141f68
sherjeelpak <sherjeelpak@gmail.com>
2025-06-11 20:34:14 +0500

    Update page.jsx

--- app/product/[equipment_id]/page.jsx
+++ app/product/[equipment_id]/page.jsx
@@ -1,7 +1,7 @@
 import EmployeeCard from '@/components/EmployeeCard';
 
 async function getProduct(equipment_id) {
-    const apiUrl = "https://7816-182-185-171-11.ngrok-free.app";
+    const apiUrl = "https://fdd9-182-185-171-11.ngrok-free.app";
     const res = await fetch(`${apiUrl}/api/products/${equipment_id}`);
     if (!res.ok) {
         throw new Error('Failed to fetch product');
